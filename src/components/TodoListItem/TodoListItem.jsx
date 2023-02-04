bae70fbc9aaac75d95bad5547a59aac62a8bae3f
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-02-04 08:16:02 +0200

    add handleMarkCompleted, handleMarkImportant

--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -2,29 +2,50 @@ import { Component } from 'react';
 import css from './TodoListItem.module.css';
 
 export default class TodoListItem extends Component {
-  render() {
-    const { label, important = false } = this.props;
+  state = {
+    done: false,
+    important: false,
+  };
 
-    const style = {
-      color: important ? 'blue' : 'black',
-      fontWeight: important ? 'bold' : 'normal',
-    };
+  handleMarkCompleted = () => {
+    this.setState(({ done }) => ({
+      done: !done,
+    }));
+  };
+
+  handleMarkImportant = () => {
+    this.setState(({ important }) => ({
+      important: !important,
+    }));
+  };
+
+  render() {
+    const { label } = this.props;
+    const { done, important } = this.state;
 
     return (
       <>
-        <span className={css.todoListItemLabel} style={style}>
+        <span
+          className={`${done ? css.todoListItemLabelDone : css.todoListItemLabel} ${
+            important ? css.todoListItemLabelImportant : css.todoListItemLabel
+          }`}
+          onClick={this.handleMarkCompleted}
+        >
           {label}
         </span>
         <div>
           <button className={css.todoListItemButton} type="button">
             delete
           </button>
-          <button className={css.todoListItemButton} type="button">
-            completed
+          <button
+            className={css.todoListItemButton}
+            type="button"
+            onClick={this.handleMarkImportant}
+          >
+            important
           </button>
         </div>
       </>
     );
   }
 }
-
