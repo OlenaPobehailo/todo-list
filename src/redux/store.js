0fd7b09941f1e1389cc2604b054c3694b47171a8
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-03-10 23:07:15 +0200

    adds redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,43 @@
 
 import { configureStore } from "@reduxjs/toolkit";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/es/storage';
 import todoReducer from './todoSlice';
 import filterReducer from './filterSlice';
 import statusReducer from './statusSlice';
 
-const store = configureStore({
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, todoReducer)
+
+
+
+export const store = configureStore({
     reducer: {
-        todo: todoReducer,
+        todo: persistedReducer,
         filter: filterReducer,
         status: statusReducer
-    }
+    },
+
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+
 });
 
-export default store;
+export const persistor = persistStore(store);
